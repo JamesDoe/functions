076cc0f5254b281397b7d1ec3c3a0f22e81ff000
JamesDoe <jamesdoejr@gmail.com>
2020-01-28 11:40:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function ToggledAtLeastOnce(_toggle) {
-    return (function () {
-        let _isToggled = false, _Toggled = _toggle;
-        return function (toggled) {
-            if (!_isToggled) {
-                if (!!_Toggled || !!toggled) return !(_isToggled = true);
-            }
-            return true;
-        };
-    })(_toggle)
+  return (function () {
+    let _isToggled = false, _Toggled = _toggle;
+    return function (toggled) {
+      if (!_Toggled && !_isToggled) {
+        if (!!toggled) _isToggled = true;
+        return false;
+      }
+      return true;
+    };
+  })();
 };
